2475e253f465270cbaa3986c923c03d4db6c3137
joounii <95225214+joounii@users.noreply.github.com>
2023-11-28 17:44:00 +0100

    a commit

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from create_video import create_new_video
 
+# text = str(input())
+# text_list = list(['test', 'test2', 'this is a sentence'])
+# print(text_list)
+
+# for text in text_list:
+#     create_new_video(text)
+    
 print("Title: ")
 title = str(input())
-
 create_new_video(title)
